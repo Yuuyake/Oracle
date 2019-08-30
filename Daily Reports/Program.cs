a2175e51fff4366b4b6fd59647e59d43a9ba97a3
Emre Ekinci <yunusemrem@windowslive.com>
2019-08-30 14:06:28 +0300

    Add files via upload

---
+++ Daily Reports/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Threading;
+
+namespace Daily_Reports {
+    class Program {
+        static void Main(string[] args) {
+            Console.Title = "OUTLOOK SEARCH";
+            var src = new Searcher();
+
+            src.RunAdvancedSearch("IMPERVA DATABASE");
+            Console.Write("\n Searching mails of \"IMPERVA DATABASE\"");
+
+            int counter = 1;
+            while (true) {
+                Console.Write("\r Searching mails of \"IMPERVA DATABASE\"" + " ".PadRight(counter%5,'.') + " ".PadRight(5- counter%5));
+                if (src.isFinished == true) {
+                    Console.Write("\r Search is DONE. Reading mails . . .                               \n\n");
+                    src.ReadMails();
+                    break;
+                }
+                else {
+                    Thread.Sleep(500);
+                    counter++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Daily Reports/Searcher.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace Daily_Reports {
+    public class Searcher {
+        public string advancedSearchTag = "Our first advanced search in Outlook";
+        public Outlook.Search advancedSearch;
+        public Outlook.Application OApp = new Outlook.Application();
+        public bool isFinished = false;
+
+        public Outlook.Search RunAdvancedSearch(string wordInSubject) {
+            string scope = "Inbox";
+            string filter = String.Format(
+                "@SQL=(\"urn:schemas:calendar:datereceived\" >= '{0:g}' " +
+                "AND \"urn:schemas:mailheader:subject\" LIKE '%{1}%')", DateTime.Today.ToString("g"), wordInSubject);
+
+            string ff1 = "urn:schemas:httpmail:datereceived >= '" + DateTime.Today.ToString("g") + "' AND " + 
+                "urn:schemas:mailheader:subject LIKE '%" + wordInSubject + "%' AND " + 
+                "urn:schemas:mailheader:sender LIKE '%" + "aaa@bbb.com" + "%'";
+            advancedSearch = null;
+            Outlook.MAPIFolder folderInbox = null;
+            Outlook.MAPIFolder folderSentMail = null;
+            Outlook.NameSpace ns = null;
+            try {
+                ns = OApp.GetNamespace("MAPI");
+                folderInbox = ns.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+                folderSentMail = ns.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderSentMail);
+                scope = "\'" + folderInbox.FolderPath + "\',\'" + folderSentMail.FolderPath + "\'";
+                advancedSearch = OApp.AdvancedSearch(scope, ff1, true, advancedSearchTag);
+                OApp.AdvancedSearchComplete += Application_AdvancedSearchComplete;
+                return advancedSearch;
+            }
+            catch (Exception ex) {
+                Console.WriteLine("\n\tException: " + ex.Message);
+                return advancedSearch;
+            }
+        }
+        public void ReadMails() {
+            Outlook.Results advancedSearchResults = null;
+            Outlook.MailItem resultItem = null;
+            StringBuilder strBuilder = null;
+            try {
+                if (advancedSearch.Tag == advancedSearchTag) {
+                    advancedSearchResults = advancedSearch.Results;
+                    if (advancedSearchResults.Count > 0) {
+                        string[] version = OApp.Version.Split('.');
+                        int hostMajorVersion = Convert.ToInt32(version[0]);
+
+                        strBuilder = new StringBuilder();
+                        strBuilder.AppendLine("Number of items found: " + advancedSearchResults.Count.ToString());
+                        Console.Write("\n [No ] " + "Subject".PadRight(40, '_') + " From".PadRight(30, '_') + " Date".PadRight(30, '_') + "\n\n");
+                        for (int i = 1; i <= advancedSearchResults.Count; i++) {
+                            resultItem = advancedSearchResults[i] as Outlook.MailItem;
+                            if (resultItem != null) {
+                                Console.WriteLine(
+                                    " [" + i.ToString().PadRight(3) + "] " +
+                                    resultItem.Subject.PadRight(41, ' ') +
+                                    resultItem.Sender.Name.PadRight(30, ' ') +
+                                    resultItem.CreationTime.ToString().PadRight(30, ' '));
+                                Marshal.ReleaseComObject(resultItem);
+                            }
+                        }
+                    }
+                    else {
+                        Console.WriteLine("There are no items found.");
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("\n\tException: " + ex.Message);
+            }
+
+            if (resultItem != null) Marshal.ReleaseComObject(resultItem);
+            if (advancedSearch != null) Marshal.ReleaseComObject(advancedSearch);
+            if (advancedSearchResults != null) Marshal.ReleaseComObject(advancedSearchResults);
+        }
+        public void Application_AdvancedSearchComplete(Outlook.Search SearchObject) {
+            isFinished = true;
+        }
+    }
+}
